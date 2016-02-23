9cb477ca541996c5a40f1a04ca2c439b706d47fa
User Ki <ki@s7.mydevil.net>
2016-02-23 12:34:56 +0100

    Dodano kompresję css oraz js

--- footer.php
+++ footer.php
@@ -25,8 +25,7 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="<?php bloginfo( 'template_url' ); ?>/js/bootstrap.min.js"></script>
-    <script src="<?php bloginfo( 'template_url' ); ?>/js/softboot.js"></script>
+    <script src="<?php bloginfo( 'template_url' ); ?>/js/scripts.min.js"></script>    
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="<?php bloginfo( 'template_url' ); ?>/js/ie10-viewport-bug-workaround.js"></script>
     <script> 
