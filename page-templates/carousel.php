166b827a94ecd637ee31964ee343b1a731713aa2
User Ki <ki@s7.mydevil.net>
2016-02-23 11:48:58 +0100

    Dodano wyświetlanie statycznych tekstów w Parent Carousel

--- page-templates/carousel.php
+++ page-templates/carousel.php
@@ -3,7 +3,21 @@
   <section id="<?php echo $post->post_name ?>"
   <?php $custom_class = get_field('custom_class'); if(!empty($custom_class)) echo 'class="'.$custom_class.'"'; ?> >
   
-    <article class="unit" <?php if(get_field('height')) echo 'data-height="'.(get_field('height')/100).'"'; ?> >
+    <article class="unit relative" <?php if(get_field('height')) echo 'data-height="'.(get_field('height')/100).'"'; ?> >
+    
+      <div class="content"> 
+        <div class="<?php the_field('content_position_content_position'); ?>">
+          <div class="content">        
+            <div class="container">      
+              <div class="row">
+                <div class="col-md-12">              
+                  <?php the_content(); ?>
+                </div>     
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>  
       
       <?php include 'snippet-carousel.php'; ?>
       
