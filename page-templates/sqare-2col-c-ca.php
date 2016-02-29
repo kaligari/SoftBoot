655bbdd96c6f4163f2f9594f9c996c5bac0d8c6c
User Ki <ki@s7.mydevil.net>
2016-02-29 16:45:00 +0100

    W szablonie 2col-ca-c dodano funkcjonalność, która w wersji mobilnej najpierw wyświetla karuzelę, a później treść

--- page-templates/sqare-2col-c-ca.php
+++ page-templates/sqare-2col-c-ca.php
@@ -1,6 +1,9 @@
 <?php /* Template Name: Square 2 Col. c-ca */ ?>
   
-  <?php $post_content = apply_filters('the_content', $post->post_content); ?> 
+  <?php
+    $post_content = apply_filters('the_content', $post->post_content);
+    $post_title = $post->post_title;
+  ?> 
          
   <section id="<?php echo $post->post_name ?>"
   class="template-square-2col-c-ca <?php $custom_class = get_field('custom_class'); if(!empty($custom_class)) echo $custom_class; ?>"
@@ -10,44 +13,50 @@
   
     <article>
       
-      <div class="container-fluid">
+      <div class="container-fluid">      
+        
+        <?php
+
+          ob_start();
+          include 'snippet-carousel.php';        
+          $snippet_carousel = ob_get_clean(); 
+
+        ?>
+        
+        <div class="col-md-6 no-padding mobile-only">
+          <div class="square-box">
+            <div class="square-box-inner no-padding">
+              <?php echo $snippet_carousel; ?>
+            </div>
+          </div>
+        </div>
                        
         <div class="col-md-6 no-padding image-no-fullwidth">
           <div class="square-box">
             <div class="square-box-inner">      
               <div class="middle">
                 <div class="content">
-                  <?php if(!get_field('content_settings_hide_title')): ?>
-                  <div class="col-md-12 page-header">
-                    <h1 class="text-center"><?php the_title(); ?></h1>
+                  <?php if(!get_field('content_settings_hide_title',$post_id)): ?>
+                  <div class="row">
+                    <div class="col-md-offset-3 col-md-6 page-header">
+                      <h1 class="text-center"><?php echo $post_title; ?></h1>
+                    </div>
                   </div>
                   <?php endif; ?>
-                  <div class="col-md-12">
-                    <?php echo $post_content ?>
-                  </div>                     
+                  <?php echo $post_content ?>                     
                 </div>
               </div>
             </div>
           </div>
         </div>
         
-        <div class="col-md-6 no-padding">
+        <div class="col-md-6 no-padding desktop-only">
           <div class="square-box">
             <div class="square-box-inner no-padding">
-              <div class="content"> 
-                <div class="<?php the_field('content_position_content_position'); ?>">
-                  <div class="content">                          
-                    <div class="col-md-12">              
-                      <?php the_field('content_2nd_content'); ?>
-                    </div>                                               
-                  </div>
-                </div>
-              </div>
-              <?php include 'snippet-carousel.php'; ?>
-            
+              <?php echo $snippet_carousel; ?>
             </div>
           </div>
-        </div>
+        </div>        
           
       </div>
       
